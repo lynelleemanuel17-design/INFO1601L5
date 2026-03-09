e390a3bca39c16f92ecc1ace34b2590b41eee2f2
lynelleemanuel17-design <lynelleemanuel17@gmail.com>
2026-03-09 14:43:59 -0400

    Refactor student data structure and related functions

--- index.js
+++ index.js
@@ -1,25 +1,101 @@
-function createPerson(name, height, weight) {
-  return { name: name, height: height, weight: weight };
+//object literal
+let bob = {
+  fname: "bob",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1603',
+      grades: [89, 34, 67]
+    },
+    {
+      course: 'INFO 1601',
+      grades: [89, 34, 67]
+    }
+  ]
+};
+
+let sally = {
+  fname: "sally",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1601',
+      grades: [100, 89, 79]
+    }
+  ]
+};
+
+let paul = {
+  fname: "paul",
+  lname: "smith",
+  age: 18,
+  height: 6,
+  transcript:[
+    {
+      course: 'INFO 1600',
+      grades: [89, 34, 67]
+    }
+  ]
+};
+
+const students = [bob, sally, paul];
+
+
+// Function 1: Get average grade for a course
+function getAverageGrade(student, course){
+  for(let item of student.transcript){
+    if(item.course === course){
+      let sum = 0;
+
+      for(let grade of item.grades){
+        sum += grade;
+      }
+
+      return sum / item.grades.length;
+    }
+  }
+  return -1;
 }
 
-function calcBMI(weight, height) {
-  return weight / (height * height);
+
+// Function 2: Get assignment mark
+function getAssignmentMark(student, course, num){
+  for(let item of student.transcript){
+    if(item.course === course){
+      return item.grades[num];
+    }
+  }
+  return -1;
 }
 
-function avgBMI(people) {
+
+// Function 3: Average assessment across students
+function averageAssessment(students, courseName, assignment){
   let sum = 0;
+  let count = 0;
 
-  for (let person of people) {
-    sum += calcBMI(person.weight, person.height);
+  for(let student of students){
+    let mark = getAssignmentMark(student, courseName, assignment);
+
+    if(mark !== -1){
+      sum += mark;
+      count++;
+    }
   }
 
-  return sum / people.length;
+  if(count === 0){
+    return -1;
+  }
+
+  return sum / count;
 }
 
-let people = [
-  createPerson("Sally", 1.6, 60),
-  createPerson("Ben", 1.81, 81),
-  createPerson("Shelly", 1.5, 50)
-];
 
-console.log(avgBMI(people));
+// Test outputs
+console.log(getAverageGrade(bob, "INFO 1603"));
+console.log(getAssignmentMark(bob, "INFO 1603", 1));
+console.log(averageAssessment(students, "INFO 1601", 0));
