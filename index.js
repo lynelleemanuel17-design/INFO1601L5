5e35ec2f18af6f026ff7fae8618a0193867c153b
lynelleemanuel17-design <lynelleemanuel17@gmail.com>
2026-03-09 14:27:51 -0400

    Update array methods examples in index.js
    Refactor array manipulation examples and remove printDate function.

--- index.js
+++ index.js
@@ -1,6 +1,79 @@
-//prints current date
-function printDate(){
-   console.log(Date().toLocaleTimeString());
+let arr = [ -5, 16, 33, 42, 103, 344];
+
+console.log(arr.includes(-5));//true
+
+arr.push(11);//adds item to the end
+
+console.log(arr)//[ -5, 16, 33, 42, 103, 344, 11];
+
+let lastItem == arr.pop();//removes last item
+
+console.log(lastItem, arr);//11, [ -5, 16, 33, 42, 103, 344]
+
+arr.unshift(22);//adds item to the front
+
+console.log(arr);//[22, -5, 16, 33, 42, 103, 344]
+
+let firstItem = arr.shift();//removes first item
+console.log(firstItem, arr);//22, [-5, 16, 33, 42, 103, 344]
+
+let reversed = arr.reverse();//creates a new array in reverse order 
+console.log(reversed);//[344, 103, 42, 33, 16, -5]
+console.log(arr.join('-'));//"-5-103-16-33-344-42" joins array with provided separator
+By default, the sort() method sorts the values as strings in alphabetical and ascending order.
+
+This works well for strings ("Apple" comes before "Banana"). However, if numbers are sorted as strings, "25" is bigger than "100", because "2" is bigger than "1".
+
+Callback-Based Array Methods
+Here are some popular array methods that take callbacks and apply them to the elements of the array in some way.
+
+forEach(): Applies a function to each element
+Filter(): Applies a function to remove elements
+map() : Applies a function to transform all elements
+some(): Applies a function to tests all elements of an array for a condition
+sort(): Applies a function to determine order
+Task 5.2
+Try the following code in the script tag of index.html and observe the output in the console tab.
+
+let arr = [12, 33, 4, 5, -4, 8, 19, 25];
+                
+//map() creates a new array from the elements of one without changing the old one
+function double(num){
+  return num * 2;
 }
-      
-setInterval(printDate, 1000);
+
+let doubledArr = arr.map(double); 
+console.log(doubledArr);
+
+function isOdd(num){
+  return element%2 !== 0; 
+}
+//Filter takes a test condition and returns only the element which 
+//make the condition true
+let odds = arr.filter(isOdd);
+console.log(odds);
+
+//Returns true or false if any of the elements of the array 
+//meets a specified condition
+function has5Factor(){
+  return ele % 5 === 0;
+}
+
+let hasFiveFactor = arr.some(has5Factor);
+console.log(hasFiveFactor);
+
+function intCompare(a, b){
+ return a - b;
+}
+
+//sort function must return either 0, +ve, -ve
+let ascending = arr.sort(intCompare)
+console.log(ascending);
+The function passed to sort must return either three type of values for two specified parameters a, b
+
++ve number if a > b
+-ve number if b > a
+0 if a === b
+Hence to sort in ascending order the function is (a, b) => a - b
+
+Back
